2e9f9fb761e6531f9e442e69bdddc46746ee4ce8
Shashank K S <shashank.s2@mphasis.com>
2023-08-29 16:20:15 +0530

    Assignment

---
+++ Assesment_13-master/Assesment_13/Models/Cricket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Assesment_13.Models
+{
+    [Table("Cricket")]
+    public class Cricket
+    {
+        [Key]
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int NoWc { get; set; }
+        
+
+    }
+}
